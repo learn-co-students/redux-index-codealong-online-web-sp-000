ffad8bf89b8fcbf8de9f5baf9750ef1449224392
Snowing222 <snowing222@gmail.com>
2021-01-21 16:41:10 -0500

    Done.

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Todo extends Component{
+    
+    render(){
+        return  (<li>{this.props.todo}</li>)
+        
+    }
+}
+
+export default Todo
+
