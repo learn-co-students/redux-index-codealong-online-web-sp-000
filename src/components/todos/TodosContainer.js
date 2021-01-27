dd3dcb1b80d8b9ef6defc6e3b7f6c23e2dde89ee
Yumacorn <ReachRyanMui@gmail.com>
2021-01-26 23:52:52 -0500

    Done.

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,36 @@
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+
+// class TodosContainer extends Component {
+ 
+//     render() {
+//       return(
+//         <div></div>
+//       )
+//     }
+
+//     
+//   }
+
+// export default connect(mapStateToProps)(TodosContainer)
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+ 
+class TodosContainer extends Component {
+ 
+  render() {
+    return(
+      <div></div>
+    );
+  }
+
+}
+ 
+const mapStateToProps = state => {
+    return {
+      todos: state.todos
+    }
+}
+
+export default connect(mapStateToProps)(TodosContainer);
