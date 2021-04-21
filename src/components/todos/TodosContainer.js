19eff871cf10246d92133afd9c486e512c47b938
Carrie Mathieu <carrieburks@gmail.com>
2021-04-21 16:50:17 -0400

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+// mapping over todos it received from Redux, passing value into child component, Todo
 class TodosContainer extends Component {
     renderTodos = () => this.props.todos.map((todo, id) => < Todo key={id} text={todo} />)
     
