878f7ddbd3ec7ab9948397e1f9b7c2266b9c8d55
A.J. Romaniello <ajiellodev@gmail.com>
2021-06-05 09:28:30 -0600

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -16,7 +16,7 @@ class CreateTodo extends Component {
     this.setState({ text: ''})
   }
 
-  handleChange(event) {
+  handleChange = event => {
     this.setState({
       text: event.target.value
     });
@@ -27,7 +27,7 @@ class CreateTodo extends Component {
       <div>
         <form onSubmit={this.handleSubmit}>
     	    <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
           <input type="submit" />
        </form>
      </div>
